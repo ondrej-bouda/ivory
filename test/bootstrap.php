194129f3d6c598e0fbddb195e3674f005af6939c
Ondrej Bouda <bouda@edookit.com>
2015-07-12 20:55:55 +0200

    rename test classes

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 // taken from https://gist.github.com/adriengibrat/4761717
 spl_autoload_register(function ($class) {
-	$file = preg_replace('#\\\|_(?!.+\\\)#','/', $class) . '.php';
+	$file = __DIR__ . '/../src/' . preg_replace('#\\\|_(?!.+\\\)#','/', $class) . '.php';
 	if (stream_resolve_include_path($file))
 		require $file;
 });
