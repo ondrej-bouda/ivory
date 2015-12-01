98b376b1396ac937eeadbec19a924d01fc9e6772
Ondrej Bouda <bouda@edookit.com>
2015-12-01 23:53:50 +0100

    add QueryRelationTest::testCompositeResult()

--- test/unit/Ivory/Relation/QueryRelationTest.php
+++ test/unit/Ivory/Relation/QueryRelationTest.php
@@ -1,6 +1,8 @@
 <?php
 namespace Ivory\Relation;
 
+use Ivory\Type\CompositeValue;
+
 class QueryRelationTest extends \Ivory\IvoryTestCase
 {
     public function testDummy()
@@ -28,7 +30,7 @@ class QueryRelationTest extends \Ivory\IvoryTestCase
 
     public function testResultAccess()
     {
-        $query = 'SELECT artist.name AS artist, array_agg(album.name ORDER BY album.year) AS album_names
+        $query = 'SELECT artist.name AS artist_name, array_agg(album.name ORDER BY album.year) AS album_names
                   FROM artist
                        JOIN album_artist ON album_artist.artist_id = artist.id
                        JOIN album ON album.id = album_artist.album_id
@@ -42,9 +44,9 @@ class QueryRelationTest extends \Ivory\IvoryTestCase
         /** @var ITuple[] $tuples */
         $tuples = iterator_to_array($qr);
         $expArray = [
-            ['artist' => 'Metallica', 'album_names' => [1 => 'Black Album', 'S & M']],
-            ['artist' => 'The Piano Guys', 'album_names' => [1 => 'The Piano Guys']],
-            ['artist' => 'Tommy Emmanuel', 'album_names' => [1 => 'Live One']],
+            ['artist_name' => 'Metallica', 'album_names' => [1 => 'Black Album', 'S & M']],
+            ['artist_name' => 'The Piano Guys', 'album_names' => [1 => 'The Piano Guys']],
+            ['artist_name' => 'Tommy Emmanuel', 'album_names' => [1 => 'Live One']],
         ];
 
         $this->assertSame(count($expArray), count($qr));
@@ -60,10 +62,10 @@ class QueryRelationTest extends \Ivory\IvoryTestCase
         $this->assertSame('S & M', $qr->value(1)[2]);
         $this->assertSame('S & M', $qr->value('album_names')[2]);
         $this->assertSame('The Piano Guys', $qr->value(0, 1));
-        $this->assertSame('The Piano Guys', $qr->value('artist', 1));
+        $this->assertSame('The Piano Guys', $qr->value('artist_name', 1));
 
         $evaluator = function (ITuple $tuple) {
-            return sprintf('%s (%d)', $tuple['artist'][0], count($tuple['album_names']));
+            return sprintf('%s (%d)', $tuple['artist_name'][0], count($tuple['album_names']));
         };
         $this->assertSame('T (1)', $qr->value($evaluator, 1));
 
@@ -73,7 +75,7 @@ class QueryRelationTest extends \Ivory\IvoryTestCase
         );
         $this->assertSame(
             ['Metallica', 'The Piano Guys', 'Tommy Emmanuel'],
-            $qr->col('artist')->toArray()
+            $qr->col('artist_name')->toArray()
         );
         $this->assertSame(
             ['M (2)', 'T (1)', 'T (1)'],
@@ -90,12 +92,37 @@ class QueryRelationTest extends \Ivory\IvoryTestCase
     {
         $conn = $this->getIvoryConnection();
         $qr = new QueryRelation($conn,
-            'SELECT artist.name AS artist, array_agg(album ORDER BY album.year) AS albums
+            'SELECT artist.name AS artist_name, array_agg(album ORDER BY album.year) AS albums
              FROM artist
                   JOIN album_artist ON album_artist.artist_id = artist.id
                   JOIN album ON album.id = album_artist.album_id
              GROUP BY artist.id
              ORDER BY artist.name'
+        );
+
+        $results = [];
+        foreach ($qr as $tuple) {
+            $albums = [];
+            foreach ($tuple['albums'] as $album) {
+                /** @var CompositeValue $album */
+                $albums[] = $album->toMap();
+            }
+            $results[$tuple['artist_name']] = $albums;
+        }
+        $this->assertSame(
+            [
+                'Metallica' => [
+                    ['id' => 2, 'name' => 'Black Album', 'year' => 1991],
+                    ['id' => 3, 'name' => 'S & M', 'year' => 1999],
+                ],
+                'The Piano Guys' => [
+                    ['id' => 1, 'name' => 'The Piano Guys', 'year' => 2012],
+                ],
+                'Tommy Emmanuel' => [
+                    ['id' => 4, 'name' => 'Live One', 'year' => 2005],
+                ],
+            ],
+            $results
         );
 
         $this->assertSame(
