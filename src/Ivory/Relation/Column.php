474e151228284e8b99e4d6a9dd39f6bab42f18f4
Ondrej Bouda <bouda@edookit.com>
2016-05-24 23:39:03 +0200

    implement Column::uniq()

--- src/Ivory/Relation/Column.php
+++ src/Ivory/Relation/Column.php
@@ -2,8 +2,13 @@
 namespace Ivory\Relation;
 
 use Ivory\Exception\NotImplementedException;
+use Ivory\Relation\Alg\CallbackValueComparator;
+use Ivory\Relation\Alg\CallbackValueHasher;
 use Ivory\Relation\Alg\ITupleEvaluator;
+use Ivory\Relation\Alg\IValueComparator;
+use Ivory\Relation\Alg\IValueHasher;
 use Ivory\Type\IType;
+use Ivory\Utils\IComparable;
 
 class Column implements \IteratorAggregate, IColumn
 {
@@ -50,7 +55,51 @@ class Column implements \IteratorAggregate, IColumn
 
     public function uniq($hasher = null, $comparator = null)
     {
-        throw new NotImplementedException();
+        if ($hasher === null) {
+            $hasher = new CallbackValueHasher(function ($value) {
+                return (is_int($value) || is_string($value) ? $value : serialize($value));
+            });
+        }
+        elseif ($hasher === 1) {
+            $hasher = new CallbackValueHasher(function ($value) {
+                return 1;
+            });
+        }
+        elseif (!$hasher instanceof IValueHasher) {
+            $hasher = new CallbackValueHasher($hasher);
+        }
+
+        if ($comparator === null) {
+            $comparator = new CallbackValueComparator(function ($a, $b) {
+                if (is_object($a) && is_object($b) && $a instanceof IComparable) {
+                    return $a->equals($b);
+                }
+                else {
+                    return ($a == $b);
+                }
+            });
+        }
+        elseif (!$comparator instanceof IValueComparator) {
+            $comparator = new CallbackValueComparator($comparator);
+        }
+
+        $hashTable = [];
+        return new FilteredColumn($this, function ($value) use ($hasher, $comparator, &$hashTable) {
+            $h = $hasher->hash($value);
+            if (!isset($hashTable[$h])) {
+                $hashTable[$h] = [$value];
+                return true;
+            }
+            else {
+                foreach ($hashTable[$h] as $v) {
+                    if ($comparator->equal($value, $v)) {
+                        return false;
+                    }
+                }
+                $hashTable[$h][] = $value;
+                return true;
+            }
+        });
     }
 
     public function toArray()
---
+++ test/unit/Ivory/Relation/UniqueColumnTest.php
@@ -0,0 +1,75 @@
+<?php
+namespace Ivory\Relation;
+
+class UniqueColumnTest extends \Ivory\IvoryTestCase
+{
+    /** @var IColumn */
+    private $baseCol;
+
+    protected function setUp()
+    {
+        $conn = $this->getIvoryConnection();
+        $qr = new QueryRelation($conn,
+            'SELECT *
+             FROM (VALUES (4, 3), (1, 2), (5, 6), (4, 4), (4, 8), (1, 1)) v (a, b)'
+        );
+        $this->baseCol = $qr->col(0);
+    }
+
+    public function testCount()
+    {
+        $this->assertSame(6, $this->baseCol->count());
+        $this->assertSame(3, $this->baseCol->uniq()->count());
+        $this->assertSame(3, $this->baseCol->uniq(1)->count());
+
+        $parity = function ($value) { return $value % 2; };
+        $this->assertSame(3, $this->baseCol->uniq($parity)->count());
+
+        $parityComp = function ($a, $b) { return $a % 2 == $b % 2; };
+        $this->assertSame(2, $this->baseCol->uniq($parity, $parityComp)->count());
+        $this->assertSame(2, $this->baseCol->uniq(1, $parityComp)->count());
+    }
+
+    public function testRename()
+    {
+        $uniq = $this->baseCol->uniq();
+        $renamed = $uniq->renameTo('x');
+
+        $this->assertSame('a', $uniq->getName());
+        $this->assertSame('x', $renamed->getName());
+        $this->assertSame(3, $renamed->count());
+    }
+
+    public function testValues()
+    {
+        $this->assertSame([4, 1, 5], $this->baseCol->uniq()->toArray());
+
+        $parity = function ($value) { return $value % 2; };
+        $this->assertSame([4, 1, 5], $this->baseCol->uniq($parity)->toArray());
+
+        $parityComp = function ($a, $b) { return $a % 2 == $b % 2; };
+        $this->assertSame([4, 1], $this->baseCol->uniq(1, $parityComp)->toArray());
+
+        $parityUniqued = $this->baseCol->uniq($parity, $parityComp);
+        $this->assertSame([4, 1], $parityUniqued->toArray());
+        $this->assertSame([4, 1], iterator_to_array($parityUniqued));
+        $this->assertSame(4, $parityUniqued->value(0));
+        $this->assertSame(1, $parityUniqued->value(1));
+        $this->assertSame(1, $parityUniqued->value(-1));
+        $this->assertSame(4, $parityUniqued->value(-2));
+
+        try {
+            $parityUniqued->value(2);
+            $this->fail();
+        }
+        catch (\OutOfBoundsException $e) {
+        }
+
+        try {
+            $parityUniqued->value(-3);
+            $this->fail();
+        }
+        catch (\OutOfBoundsException $e) {
+        }
+    }
+}
