78953ca2ec93cb4d3573a61bc53e9c12d1a23ede
Ondrej Bouda <bouda@edookit.com>
2017-03-24 00:48:14 +0100

    performance test script on all supported configurations

---
+++ test/perf/scenario/perf.bat
@@ -0,0 +1,22 @@
+@echo php bench.php --recreate --rounds 100 --warmup 5 --busyloop 30000 IMPL > perf.txt
+@echo measured on ThinkPad T540p >> perf.txt
+
+@echo( >> perf.txt
+@echo pgsql: >> perf.txt
+php bench.php --recreate --rounds 100 --warmup 5 --busyloop 30000 pgsql >> perf.txt
+
+@echo( >> perf.txt
+@echo ivory: >> perf.txt
+php bench.php --recreate --rounds 100 --warmup 5 --busyloop 30000 ivory >> perf.txt
+
+@echo( >> perf.txt
+@echo ivory-sync: >> perf.txt
+php bench.php --recreate --rounds 100 --warmup 5 --busyloop 30000 ivory-sync >> perf.txt
+
+@echo( >> perf.txt
+@echo dibi: >> perf.txt
+php bench.php --recreate --rounds 100 --warmup 5 --busyloop 30000 dibi >> perf.txt
+
+@echo( >> perf.txt
+@echo dibi-lazy: >> perf.txt
+php bench.php --recreate --rounds 100 --warmup 5 --busyloop 30000 dibi-lazy >> perf.txt
