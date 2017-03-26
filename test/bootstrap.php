57b6264b4fb1d95c39672360b1419a8fe39779df
Ondrej Bouda <bouda@edookit.com>
2017-03-26 21:20:44 +0200

    test bootstrap exception handler dumping the exception to stderr

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -16,3 +16,15 @@ spl_autoload_register(function ($class) {
 });
 
 require_once __DIR__ . '/../lib/composed/autoload.php';
+
+set_exception_handler(function (Throwable $t) {
+    fprintf(STDERR,
+        "Uncaught %s: %s\nThrown from %s:%d\n%s\n",
+        get_class($t),
+        $t->getMessage(),
+        $t->getFile(),
+        $t->getLine(),
+        $t->getTraceAsString()
+    );
+    exit(2);
+});
