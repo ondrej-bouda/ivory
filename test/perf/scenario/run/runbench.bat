9fa091eb91844ae8c15202ca4cdc6605ced9da3b
Ondrej Bouda <bouda@edookit.com>
2017-04-10 22:39:31 +0200

    fix runbench.bat to run all the subtests

--- test/perf/scenario/run/runbench.bat
+++ test/perf/scenario/run/runbench.bat
@@ -1,9 +1,11 @@
 @echo php bench.php --recreate --rounds 100 --warmup 5 --busyloop 100000 IMPL > ../out/runbench.out
 @echo measured on ThinkPad T540p >> ../out/runbench.out
 
-runbench_pgsql.cmd
-runbench_ivory.cmd
-runbench_ivory-sync.cmd
-runbench_dibi.cmd
-runbench_dibi-lazy.cmd
-runbench_doctrine.cmd
+@call runbench_pgsql.cmd
+@call runbench_ivory.cmd
+@call runbench_ivory-sync.cmd
+@call runbench_ivory-nocache.cmd
+@call runbench_dibi.cmd
+@call runbench_dibi-lazy.cmd
+@call runbench_doctrine.cmd
+@call runbench_laravel.cmd
