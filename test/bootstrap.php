bf3128c9e06fc16fc109fc54a6bea53c7a76f95e
Ondrej Bouda <bouda@edookit.com>
2017-09-23 16:07:55 +0200

    complete composer.json, setup autoloading, simplify test/bootstrap.php

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,20 +1,4 @@
 <?php
-// adapted from https://gist.github.com/adriengibrat/4761717
-spl_autoload_register(function ($class) {
-    $roots = [
-        __DIR__ . '/../src/',
-        __DIR__ . '/unit/',
-    ];
-    $filename = preg_replace('#\\\|_(?!.+\\\)#', '/', $class) . '.php';
-    foreach ($roots as $root) {
-        $path = $root . $filename;
-        if (stream_resolve_include_path($path)) {
-            require $path;
-            return;
-        }
-    }
-});
-
 require_once __DIR__ . '/../lib/composed/autoload.php';
 
 set_exception_handler(function (Throwable $t) {
