fd9fb868eb3ed60bec116f2f79d946fae92aba6f
Ondrej Bouda <bouda@edookit.com>
2018-02-21 21:07:24 +0100

    document EnumType

--- src/Ivory/Type/Postgresql/EnumType.php
+++ src/Ivory/Type/Postgresql/EnumType.php
@@ -7,13 +7,28 @@ use Ivory\NamedDbObject;
 use Ivory\Type\ITotallyOrderedType;
 use Ivory\Value\EnumItem;
 
+/**
+ * Generic enumeration type converter.
+ *
+ * When parsing enumeration values from PostgreSQL, objects of {@link EnumItem} are produced, which hold both the type
+ * name and the order of the value within the enumeration values (recall that in PostgreSQL, enumeration values are
+ * compared based on their position within the defined enum). The `EnumItem` objects are comparable, and even ranges of
+ * enum values (of the same enumeration type) may be constructed.
+ *
+ * The complexity of the `EnumItem` objects comes at a cost, however: to construct an enumeration value, one must use
+ * the {@link EnumItem::forType()} factory method and give it, besides the actual value, also the schema name, the type
+ * name and the ordinal. To remedy this problem, plain strings are also accepted by this type converter when serializing
+ * a value to PostgreSQL; still, the given string is checked whether among the values defined by the enumeration, and a
+ * warning is raised if not. Also note the {@link EnumItem::getValue()} method, returning the enum value as a string,
+ * which is also returned when simply typecasting an `EnumItem` to a string.
+ */
 class EnumType implements ITotallyOrderedType
 {
     use NamedDbObject;
 
     private $labelSet;
 
-    public function __construct(string $schemaName, string $typeName, $labels)
+    public function __construct(string $schemaName, string $typeName, array $labels)
     {
         $this->setName($schemaName, $typeName);
         $this->labelSet = array_flip($labels);
